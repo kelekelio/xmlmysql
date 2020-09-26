5eed5f3edb534e5a7a9cc5f06566b63aaca95b8d
kelekelio <cristal01@gmail.com>
2020-09-26 12:33:15 +0200

    Create Configuration.java

---
+++ src/main/java/Extra/Configuration.java
@@ -0,0 +1,16 @@
+package Extra;
+
+/**
+ * @author Grzegorz Nowakowski
+ */
+public class Configuration {
+    //FTP
+    public static final String FTP_HOST = "FTP_HOST";
+    public static final String FTP_USER = "FTP_USER";
+    public static final String FTP_PASS = "FTP_PASS";
+    public static final int FTP_PORT = 21;
+
+
+    //Discord
+    public static final String DISCORD_WEBHOOK = "DISCORD_WEBHOOK";
+}
