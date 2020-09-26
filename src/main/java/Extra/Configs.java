c0a74dea49c613630358a354f5ae078923cac5d0
kelekelio <cristal01@gmail.com>
2020-09-26 12:01:51 +0200

    ignore test

--- src/main/java/Extra/Configs.java
+++ src/main/java/Extra/Configs.java
@@ -3,13 +3,4 @@ package Extra;
 /**
  * @author Grzegorz Nowakowski
  */
-public class Configs {
-    // FTP
-    public static final String  FTP_HOST = "FTP_HOST";
-    public static final String  FTP_USER = "FTP_USER";
-    public static final String  FTP_PASS = "FTP_PASS";
-    public static final int     FTP_PORT = 21;
-
-    // Discord
-    public static final String  DISCORD_WEBHOOK = "DISCORD_WEBHOOK";
-}
+public class Configs {}
