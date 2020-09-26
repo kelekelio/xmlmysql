9aa5533e9f88ad7f9298ead55380b68e683b427d
kelekelio <cristal01@gmail.com>
2020-09-26 12:42:02 +0200

    Update Configuration.java

--- src/main/java/Extra/Configuration.java
+++ src/main/java/Extra/Configuration.java
@@ -9,7 +9,20 @@ public class Configuration {
     public static final String FTP_USER = "FTP_USER";
     public static final String FTP_PASS = "FTP_PASS";
     public static final int FTP_PORT = 21;
+    
+    //SSH
+    public static final String SSH_KEY = "C:\ssh_private_key.ppk";
+    public static final int SSH_PORT = 1;
+    public static final String SSH_USER = "SSH_USER";
+    public static final String SSH_PASS = "SSH_PASS";
+    public static final String SSH_HOST = "SSH_HOST";
 
+    //DB
+    public static final String DB_NAME = "DB_NAME";
+    public static final String DB_USER = "DB_USER";
+    public static final String DB_PASS = "DB_PASS";
+    public static final String DB_HOST = "localhost";
+    public static final int DB_PORT = 3306;
 
     //Discord
     public static final String DISCORD_WEBHOOK = "DISCORD_WEBHOOK";
